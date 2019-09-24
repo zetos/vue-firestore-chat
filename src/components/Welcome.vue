7df835d10832ed1c3bc1b36d21994b45a7782965
Zetos Lima <cesar_manoel@hotmail.com>
2019-09-23 22:36:15 -0300

    Welcome enterChat function

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,6 +6,7 @@
         <form @submit.prevent="enterChat">
           <label for="name">Enter your name:</label>
           <input type="text" name="name" v-model="name" />
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
           <button class="btn teal">Enter Chat</button>
         </form>
       </div>
@@ -18,12 +19,18 @@ export default {
   name: 'Welcome',
   data() {
     return {
-      name: null
+      name: null,
+      feedback: null
     };
   },
   methods: {
     enterChat() {
-      console.log(this.name);
+      if (this.name) {
+        this.feedback = null;
+        this.$router.push({ name: 'chat', params: { name: this.name } });
+      } else {
+        this.feedback = 'You must enter a name to join';
+      }
     }
   }
 };
